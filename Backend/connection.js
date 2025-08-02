bbbe6e3d3419358e5c199535f93ac9086d6cdb36
Johan-Joseph <josephjohan040@gmail.com>
2025-08-02 20:52:25 +0530

    first commit

---
+++ Backend/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb://localhost:27017/blogapp"
+  )
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
